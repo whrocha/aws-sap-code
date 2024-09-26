50b5678201e849e5d9f8fca71c646ead6d932f92
Your Name <you@example.com>
2024-09-26 13:36:14 +0200

    updated serverless app

---
+++ build-a-serverless-app/Part-3/DCTProductVisitForm/backend/index.js
@@ -0,0 +1,37 @@
+'use strict'
+const AWS = require('aws-sdk')
+const uuidv4 = require('uuid/v4')
+
+AWS.config.update({ region: process.env.Region, apiVersion: '2012-08-10' })
+
+const sqsClient = new AWS.SQS({apiVersion: '2012-11-05'})
+
+module.exports.handler = async (event) => {
+  
+  console.log('Received event:', JSON.parse(event.body));
+  
+  try {
+
+    var params = {
+      MessageBody: event.body,
+
+      QueueUrl: "https://sqs.us-east-1.amazonaws.com/411382599149/ProductVisitsDataQueue"
+    };
+
+    const data = await sqsClient.sendMessage(params).promise();
+    
+    return {
+        statusCode: 200,
+        headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
+        body: JSON.stringify({ status: 'success', messageId: data.MessageId })
+      }
+    } catch (err) {
+        console.error(err)
+        return {
+            statusCode: 200,
+            headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
+            status: 'error',
+            message: err
+        }
+    }
+}
